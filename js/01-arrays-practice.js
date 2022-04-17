61fe529246de31f271d55a9cb888e97367f891d4
d-herbert <deidre.m.herbert@gmail.com>
2022-04-17 13:52:39 -0700

    steps 8-12 done

--- js/01-arrays-practice.js
+++ js/01-arrays-practice.js
@@ -30,15 +30,34 @@ for (let movie of movies.sort()) {
     console.log(movie)
 }
 //STEP 8
-
+movies = ['Clueless', 'Office Space', 'A Star Is Born', 'Spirited Away', 'Bridesmaids', 'Moonstruck', 'Saved']
+leastFavMovies = ['Little Man', 'Gotti', 'The Boss Baby']
+console.log('Movies I like: \n\n')
+for (let movie in movies) {
+    console.log(movies[movie])
+}
+console.log('\n\nMovies I regret watching: \n\n')
+for (let movie in leastFavMovies) {
+    console.log(leastFavMovies[movie])
+}
 //STEP 9
-
+moviesAll = movies.concat(leastFavMovies)
+console.log(moviesAll.sort().reverse())
 //STEP 10
-
+moviesAll = movies.concat(leastFavMovies)
+console.log(moviesAll.reverse()[0])
 //STEP 11
-
+moviesAll = movies.concat(leastFavMovies)
+console.log(moviesAll[0])
 //STEP 12
-
+console.log('new ----------')
+let m1 = moviesAll.indexOf('Little Man')
+let m2 = moviesAll.indexOf('Gotti')
+let m3 = moviesAll.indexOf('The Boss Baby')
+moviesAll[m1] = 'Garden State'
+moviesAll[m2] = 'Best in Show'
+moviesAll[m3] = 'Elf'
+console.log(moviesAll)
 //STEP 13
 
 //STEP 14
